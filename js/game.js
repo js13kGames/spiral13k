51c40c7ab68d99da8cdac6958ce2afac5cadf021
Kurakute Kurai <haet.fake.2.0@gmail.com>
2014-09-07 22:03:21 +0400

    #2

--- js/game.js
+++ js/game.js
@@ -10,8 +10,7 @@ var bBLOCK_G = false;
 var moonOrbitR = 45;
 var moonMotionPath = 75;
 var angle = 0.75 * Math.PI;
-var blockAngle = Math.PI/2;
-var blockXY = [];
+var blocks = [];
 var d0 = [];
 var meteor = [];
 
@@ -46,10 +45,6 @@ bg_ctx.arc( centerX, centerY, 10, 0, 2*Math.PI );
 bg_ctx.fillStyle = 'rgba(61,204,248,0.2)';
 bg_ctx.fill();
 bg_ctx.stroke();
-/*bg_ctx.beginPath();
-bg_ctx.arc( centerX, centerY, 20, 0, 2*Math.PI );
-bg_ctx.stroke();*/
-
 
 
 function start() {
@@ -64,6 +59,59 @@ function start() {
 		position: new Vec(px, py),
 		velocity: new Vec(vx, vy)
 	};
+
+	blocks = [];
+	blocks.push({
+		position:{
+			x: centerX + 220 * Math.cos(0 * Math.PI / 180),
+			y: centerY + 220 * Math.sin(0 * Math.PI / 180)
+		},
+		delta: { x: 220, y: -220 },
+		radius:  40,
+		direction: -1,
+		angle: 90,
+		limit: { min: 20, max: 160 },
+		color: '0,0,0'
+	});
+
+	blocks.push({
+		position:{
+			x: centerX + 220 * Math.cos(0 * Math.PI / 180),
+			y: centerY - 220 * Math.sin(0 * Math.PI / 180)
+		},
+		delta: { x: 220, y: -220 },
+		radius:  40,
+		direction: -1,
+		angle: 270,
+		limit: { min: 200, max: 340 },
+		color: '0,0,0'
+	});
+
+	blocks.push({
+		position:{
+			x: centerX + 420 * Math.cos(0 * Math.PI / 180),
+			y: centerY - 320 * Math.sin(0 * Math.PI / 180)
+		},
+		delta: { x: 420, y: -320 },
+		radius:  40,
+		direction: 1,
+		angle: -60,
+		limit: { min: -60, max: 60 },
+		color: '0,0,0'
+	});
+
+	blocks.push({
+		position:{
+			x: centerX + 420 * Math.cos(0 * Math.PI / 180),
+			y: centerY - 320 * Math.sin(0 * Math.PI / 180)
+		},
+		delta: { x: 420, y: -320 },
+		radius:  40,
+		direction: -1,
+		angle: 120,
+		limit: { min: 120, max: 240 },
+		color: '0,0,0'
+	});
 
 	requestAnimationFrame(render);
 }
@@ -105,6 +153,66 @@ function render () {
 	if(bGAME) requestAnimationFrame(render);
 }
 
+var blocksStep = function(){
+	var ib = blocks.length;
+	var angle_direction = 1;
+	var crossing = false;
+
+	for (var i = ib - 1; i >= 0; i--) {
+		blocks[i].position.x = centerX + blocks[i].delta.x * Math.cos(blocks[i].angle * Math.PI / 180);
+		blocks[i].position.y = centerY + blocks[i].delta.y * Math.sin(blocks[i].angle * Math.PI / 180);
+
+		var 
+			bx = Math.pow( blocks[i].position.x - meteor.position.x, 2), 
+			by = Math.pow( blocks[i].position.y - meteor.position.y, 2);
+
+		if(Math.sqrt(bx+by) <= blocks[i].radius){
+			blocks[i].color = '0,255,0';	
+			crossing = new Vec( blocks[i].position.x,blocks[i].position.y );
+		} else {
+			blocks[i].color = '0,0,0';
+		}
+
+		var b_grd_radial = ctx.createRadialGradient(
+				blocks[i].position.x,blocks[i].position.y,
+				10,
+				blocks[i].position.x,blocks[i].position.y,
+				blocks[i].radius
+			);
+			b_grd_radial.addColorStop(0.40, 'rgba(255,255,255,0)');
+			b_grd_radial.addColorStop(0.75, 'rgba('+blocks[i].color+',.1)');
+			b_grd_radial.addColorStop(0.80, 'rgba('+blocks[i].color+',.25)');
+			b_grd_radial.addColorStop(0.85, 'rgba('+blocks[i].color+',.1)');
+			b_grd_radial.addColorStop(1.00, 'rgba(255,255,255,0)');
+
+		ctx.beginPath();
+		ctx.arc( blocks[i].position.x, blocks[i].position.y, blocks[i].radius+5, 0, dPI );
+		ctx.fillStyle = b_grd_radial;
+		ctx.fill();
+
+		ctx.beginPath();
+		ctx.strokeStyle = 'rgba('+blocks[i].color+',.15)';
+		ctx.fillStyle = 'rgba('+blocks[i].color+',.25)';
+		ctx.lineWidth = 5;
+		ctx.arc( blocks[i].position.x, blocks[i].position.y, 15, 0, dPI );
+		ctx.fill();
+		ctx.stroke();
+
+		ctx.beginPath();
+		ctx.strokeStyle = 'rgba('+blocks[i].color+',.2)';
+		ctx.lineWidth = 1;
+		ctx.arc( blocks[i].position.x, blocks[i].position.y, 35, 0, dPI );
+		ctx.stroke();
+
+		blocks[i].angle -= (blocks[i].direction * 0.42);
+		if(blocks[i].angle > blocks[i].limit.max || blocks[i].angle < blocks[i].limit.min) blocks[i].direction *= -1;
+	};
+
+	bBLOCK_G = (crossing === false) ? false : true;
+
+	return crossing;
+}
+
 var meteorStep = function(center, body){
 	delta = 1;
 	var d1 = compute(center, body, delta*0, d0);
@@ -138,17 +246,20 @@ var motion = function() {
 	if(meteor.position.y > canvas.height || meteor.position.y < 0)
 		meteor.velocity.y *= -1;
 
-	meteorStep(center, meteor);	
+	var crossing = blocksStep();
+	center = crossing ? crossing : center;
+	meteorStep( center, meteor);	
 
-		// Meteor[path]
-	/*bg_ctx.strokeStyle = 'rgba(255,64,64,0.8)';
+	// Meteor[path]
+	/*
+	bg_ctx.strokeStyle = 'rgba(255,64,64,0.8)';
 	bg_ctx.lineWidth = 1;
 	bg_ctx.beginPath();
 	bg_ctx.setLineDash([2, 3]);
 	bg_ctx.moveTo(previous.x, previous.y);
 	bg_ctx.lineTo(meteor.position.x, meteor.position.y);
 	bg_ctx.stroke();
-*/
+	*/
 	// Meteor
 	ctx.beginPath();
 	ctx.lineWidth = 1;
@@ -167,6 +278,30 @@ var motion = function() {
 
 };
 
+
+// ...
+canvas.onmousemove = function(move){
+	angle = Math.atan2(move.offsetY - centerY, move.offsetX - centerX) * 180/Math.PI;
+}
+
+window.onkeydown = function (press) {	
+	if(press.keyCode == 83) {
+		if(!bGAME && !bPAUSE){
+			bGAME = true;
+			start();
+		}
+
+	} else if(press.keyCode == 80) {
+		bGAME = bGAME ? false : true;
+		bPAUSE = bPAUSE ? false : true;
+		if(bGAME) requestAnimationFrame(render);
+	}
+};
+
+/*
+* http://codeflow.org/entries/2010/aug/28/integration-by-example-euler-vs-verlet-vs-runge-kutta/
+* 
+*/
 var Derivative = function(position, velocity){
 	this.position = position ? position : new Vec(0, 0);
 	this.velocity = velocity ? velocity : new Vec(0, 0);
@@ -305,21 +440,3 @@ var copy = function(){
 	return result;
 }
 
-// ...
-canvas.onmousemove = function(move){
-	angle = Math.atan2(move.offsetY - centerY, move.offsetX - centerX) * 180/Math.PI;
-}
-
-window.onkeydown = function (press) {	
-	if(press.keyCode == 83) {
-		if(!bGAME && !bPAUSE){
-			bGAME = true;
-			start();
-		}
-
-	} else if(press.keyCode == 80) {
-		bGAME = bGAME ? false : true;
-		bPAUSE = bPAUSE ? false : true;
-		if(bGAME) requestAnimationFrame(render);
-	}
-};
